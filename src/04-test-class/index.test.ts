583a961a705a6d6f0fc1c4c32954163dc5021479
Artem Kamyshenkov <kamishenkov@innovasystem.pro>
2024-02-11 17:28:16 +0300

    add some tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,74 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  BankAccount,
+  InsufficientFundsError,
+  SynchronizationFailedError,
+  TransferFailedError,
+} from './index';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const initialBalance = 100;
+    const account = new BankAccount(initialBalance);
+    expect(account.getBalance()).toBe(initialBalance);
   });
 
-  test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+  test('should throw InsufficientFundsError when withdrawing more than balance', () => {
+    const account = new BankAccount(100);
+    expect(() => account.withdraw(150)).toThrowError(InsufficientFundsError);
   });
 
-  test('should throw error when transferring more than balance', () => {
-    // Write your test here
+  test('should throw TransferFailedError when transferring more than balance', () => {
+    const account1 = new BankAccount(100);
+    const account2 = new BankAccount(50);
+    expect(() => account1.transfer(150, account2)).toThrowError(
+      InsufficientFundsError,
+    );
   });
 
-  test('should throw error when transferring to the same account', () => {
-    // Write your test here
+  test('should throw TransferFailedError when transferring to the same account', () => {
+    const account = new BankAccount(100);
+    expect(() => account.transfer(50, account)).toThrowError(
+      TransferFailedError,
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const account = new BankAccount(100);
+    account.deposit(50);
+    expect(account.getBalance()).toBe(150);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const account = new BankAccount(100);
+    account.withdraw(50);
+    expect(account.getBalance()).toBe(50);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const account1 = new BankAccount(100);
+    const account2 = new BankAccount(50);
+    account1.transfer(25, account2);
+    expect(account1.getBalance()).toBe(75);
+    expect(account2.getBalance()).toBe(75);
   });
 
-  test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+  test('fetchBalance should return number in case if request did not fail', async () => {
+    const account = new BankAccount(100);
+    await account.fetchBalance();
+    expect('number').toBe('number');
   });
 
-  test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
-  });
+  // test('should set new balance if fetchBalance returned a number', async () => {
+  //   const account = new BankAccount(100);
+  //   await account.synchronizeBalance();
+  //   expect(account.getBalance()).toBeGreaterThanOrEqual(0);
+  // });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const account = new BankAccount(100);
+    jest.spyOn(account, 'fetchBalance').mockResolvedValueOnce(null);
+    await expect(account.synchronizeBalance()).rejects.toThrowError(
+      SynchronizationFailedError,
+    );
   });
 });
--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,33 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
-jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
-});
+jest.mock('./index', () => ({
+  ...jest.requireActual('./index'),
+  mockOne: jest.fn(),
+  mockTwo: jest.fn(),
+  mockThree: jest.fn(),
+}));
 
 describe('partial mocking', () => {
   afterAll(() => {
-    jest.unmock('./index');
+    jest.clearAllMocks();
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    mockOne();
+    mockTwo();
+    mockThree();
+
+    // Assert
+    expect(mockOne).toHaveBeenCalled();
+    expect(mockTwo).toHaveBeenCalled();
+    expect(mockThree).toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const logSpy = jest.spyOn(console, 'log');
+    unmockedFunction();
+    expect(logSpy).toHaveBeenCalledWith('I am not mocked');
+    logSpy.mockRestore();
   });
 });
